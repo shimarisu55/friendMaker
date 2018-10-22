f2813d21952912de50f74e25bf10abf98349ccbe
yukidaru55 <shiho.2480@gmail.com>
2018-10-22 09:46:38 +0000

    三回質問が終わった後ゴールページへ行きます

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   resources :users,only: [:index,:show,:edit,:update,:destroy] do
     resources :questions,only: [:show,:new,:create, :update,:edit]
     resources :answers,only: [:create,:edit,:update]
+    get 'goal'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
